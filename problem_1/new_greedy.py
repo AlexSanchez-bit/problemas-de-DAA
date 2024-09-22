34237770c713d6bca41cb36f3cbd76c5f2b3683c
aspio28 <aspiolea28@gmail.com>
2024-09-22 19:30:11 -0400

    fix new greedy

--- problem_1/new_greedy.py
+++ problem_1/new_greedy.py
@@ -1,4 +1,4 @@
-from utils.problem1 import marcar_rectangulo,matriz_esta_vacia,eliminar_rectangulo,peso_rectangulo, rectangulo_borrado,rect_a_contenido,imprimir_matriz
+from utils.problem1 import area_rect,matriz_esta_vacia,eliminar_rectangulo,peso_rectangulo, rectangulo_borrado,rect_a_contenido,imprimir_matriz
 
 def cuantos_negros(matriz, rectangulo):
     negros = 0
@@ -10,15 +10,13 @@ def cuantos_negros(matriz, rectangulo):
 
 def greedy_max_area_upgrade(matriz, rectangulos: list):
     response=[]
-    # rectangulos.sort(key = lambda rect: ((abs(rect[0] - rect[2]) + 1) * (abs(rect[1] - rect[3]) + 1)),reverse=True)
     
-    rectangulos_with_area = [(rect, ((abs(rect[0] - rect[2]) + 1) * (abs(rect[1] - rect[3]) + 1))) for rect in rectangulos]
+    rectangulos_with_area = [(rect, area_rect(rect[0],rect[1],rect[2],rect[3])) for rect in rectangulos]
     rectangulos_with_area.sort(key = lambda rect: rect[1], reverse=True)
-    # print([(rect,((abs(rect[0] - rect[2]) + 1) * (abs(rect[1] - rect[3]) + 1))) for rect in rectangulos])
 
     costo=0
     while(not matriz_esta_vacia(matriz) ):
-        biggest_rect =rectangulos_with_area.pop(0)[0]
+        biggest_rect, area_max =rectangulos_with_area.pop(0)
         #si ya fue borrado totalmente por el resto se ignora
         x1,y1,x2,y2 = biggest_rect
         if rectangulo_borrado(matriz,x1,y1,x2,y2):
@@ -31,8 +29,10 @@ def greedy_max_area_upgrade(matriz, rectangulos: list):
         for (rect, area) in rectangulos_with_area:
             negros = cuantos_negros(matriz, rect)
             new_rectangulos_with_area.append((rect, negros))
-        new_rectangulos_with_area.sort(key = lambda rect: rect[1], reverse=True)
+
+        new_rectangulos_with_area.sort(key = lambda rect: (rect[1], peso_rectangulo(rect[0][0], rect[0][1], rect[0][2], rect[0][3])), reverse=True)
         rectangulos_with_area = new_rectangulos_with_area
+        
         response.append(biggest_rect)
         costo += peso_rectangulo(x1,y1,x2,y2)
     
